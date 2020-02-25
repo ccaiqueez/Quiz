7a6eb4b8efc10d0a237834a675cd61cb6c023704
Silvestre Caíque <silvestre.engcomp@gmail.com>
2020-02-25 07:33:08 -0300

    Criação das rotas da Categoria

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,9 +3,15 @@ const routes = Router()
 
 const AuthMiddleware = require('./middleware/Auth')
 const UserController = require('./controller/UserController')
+const CategoryController = require('./controller/CategoryController')
 
 routes.post('/user', UserController.store)
 
 routes.use(AuthMiddleware)
+
+routes.post('/category', CategoryController.store)
+routes.get('/category', CategoryController.index)
+routes.put('/category/:id', CategoryController.update)
+routes.delete('/category/:id', CategoryController.delete)
 
 module.exports = routes
