6d16ef9ee947e70b7a35f4f3000ae04c0e5eb4d4
Silvestre Caíque <silvestre.engcomp@gmail.com>
2020-02-25 07:28:18 -0300

    Criação dos metodos do CategoryController

--- backend/src/controller/CategoryController.js
+++ backend/src/controller/CategoryController.js
@@ -1,9 +1,38 @@
-const modelCategory = require('../model/Category')
+const ModelCategory = require('../model/Category')
+const uuid = require('uuid')
 
 module.exports = {
     async store(req, res){
-        
+        const id = uuid()        
+        const { user, name } = req.body
 
-        const cate
+        const category = await ModelCategory.create({
+            id, user, name
+        })
+
+        return category ? res.status(201).json(category) : res.status(404).json({error: 'Unable to create Category'})
+    },
+    async index(req, res){
+        const { user } = req.body
+
+        const category = await ModelCategory.findAll({ where: { user }})
+
+        return category ? res.status(200).json(category) : res.status(404).json({error: 'Could not find Category'})
+    },
+    async update(req, res){
+        const { id } = req.query
+        const { name } = req.body
+
+        const category = await ModelCategory.update({ 
+            name, where: { name } })
+
+        return category ? res.status(200).json(category) : res.status(404).json({error: 'Unable to update data of Category'})
+    },
+    async delete(req, res){
+        const { id } = req.query
+
+        const category = await ModelCategory.destroy({ where: { id } })
+
+        return category ? res.status(200).json({success: 'Successful category exclusion'}) : res.status(404).json({error: 'Unable to delete category'})
     }
 }
